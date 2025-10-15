7c796708aea1c2b5dab2a1546e1914255012a2a8
snowballons <snowballons@protonmail.com>
2025-10-15 17:48:30 +0300

    Add TinaCMS token

--- tina/config.js
+++ tina/config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "tinacms";
 export default defineConfig({
   branch: "main",
   clientId: "eaf14aa4-251d-4bbf-8813-448dc87b9274", // Get this from tina.io
-  token: null, // Get this from tina.io
+  token: "f309ad3b15e8a456f91236fe1a682d4d1c024d1d", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
