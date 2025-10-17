9e097cec74c6594589b6bc58c8942266c59d868d
snowballons <snowballons@protonmail.com>
2025-10-17 10:41:13 +0300

    Update TinaCMS schema files to sync with TinaCloud

--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -416,7 +416,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "https://content.tinajs.io/1.6/content/eaf14aa4-251d-4bbf-8813-448dc87b9274/github/main",
+        url: "http://localhost:4001/graphql",
         queries,
       })
     )
