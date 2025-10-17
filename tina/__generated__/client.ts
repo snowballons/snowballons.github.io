69ad0a2d531482126dbec6cc21dce59c17529485
snowballons <snowballons@protonmail.com>
2025-10-17 10:32:10 +0300

    Consolidate to single repo with TinaCMS search

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/jonnie/Documents/projects-2/snowballons-site/tina/__generated__/.cache/1760685819742', url: 'https://content.tinajs.io/1.6/content/eaf14aa4-251d-4bbf-8813-448dc87b9274/github/main', token: 'f309ad3b15e8a456f91236fe1a682d4d1c024d1d', queries,  });
+export const client = createClient({ cacheDir: '/home/jonnie/Documents/projects-2/snowballons-site/tina/__generated__/.cache/1760686023516', url: 'https://content.tinajs.io/1.6/content/eaf14aa4-251d-4bbf-8813-448dc87b9274/github/main', token: 'f309ad3b15e8a456f91236fe1a682d4d1c024d1d', queries,  });
 export default client;
   
